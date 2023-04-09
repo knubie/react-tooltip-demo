852eba3e5d21fc88089e0ba6189177015a9ddfef
Matthew Steedman <steedman87@gmail.com>
2023-04-09 15:07:15 -0400

    Add react-tooltip demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
+import { Tooltip } from 'react-tooltip';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        data-tooltip-id="foobar"
+        data-tooltip-content="foo"
+        data-tooltip-place="right"
+        className="sidebar-item"
+      >
+        foo
+      </div>
+      <div
+        data-tooltip-id="foobar"
+        data-tooltip-content="bar"
+        data-tooltip-place="right"
+        className="sidebar-item"
+      >
+        bar
+      </div>
+      <div
+        data-tooltip-id="foobar"
+        data-tooltip-content="bizz"
+        data-tooltip-place="right"
+        className="sidebar-item"
+      >
+        bizz
+      </div>
+      <Tooltip id="foobar" delayHide={0} className="tooltip"/>
     </div>
   );
 }
